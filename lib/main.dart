b5169a8aa17bc220acab265c09e4aa9374800ddf
LeoCosta <leocosta.black@hotmail.com>
2022-12-24 23:14:09 -0300

    Create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:my_contacts/pages/home/home_page.dart';
 
 void main() {
-  runApp(Container());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
