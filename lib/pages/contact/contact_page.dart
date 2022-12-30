96e2ebbbb08afd6ab807d79686ebee32f858f5fd
LeoCosta <leocosta.black@hotmail.com>
2022-12-30 17:34:08 -0300

    Create confirm page pop

--- lib/pages/contact/contact_page.dart
+++ lib/pages/contact/contact_page.dart
@@ -49,90 +49,131 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_isEditedContact ? 'Edit contact' : 'Create contact'),
-        centerTitle: true,
-        backgroundColor: darkLightColor,
-      ),
-      backgroundColor: darkColor,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Verifica se os campos estão preenchidos
-          if (_editedContact.name.isNotEmpty) {
-            if (_editedContact.email.isNotEmpty || _editedContact.phone.isNotEmpty) {
-              print('oi');
-              Navigator.pop(context, _editedContact);
-            } else {
-              FocusScope.of(context).requestFocus(inputPhoneFocusNode);
-            }
-          } else {
-            // Foca no input "Name"
-            FocusScope.of(context).requestFocus(inputNameFocusNode);
-          }
-        },
-        backgroundColor: mainColor,
-        child: const Icon(
-          Icons.save,
-          color: darkColor,
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_isEditedContact ? 'Edit contact' : 'Create contact'),
+          centerTitle: true,
+          backgroundColor: darkLightColor,
         ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            GestureDetector(
-              child: Container(
-                width: 140,
-                height: 140,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: _editedContact.imageDirectory != null
-                        ? FileImage(File(_editedContact.imageDirectory!))
-                        : const AssetImage('images/person.png') as ImageProvider,
+        backgroundColor: darkColor,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Verifica se os campos estão preenchidos
+            if (_editedContact.name.isNotEmpty) {
+              if (_editedContact.email.isNotEmpty || _editedContact.phone.isNotEmpty) {
+                Navigator.pop(context, _editedContact);
+              } else {
+                FocusScope.of(context).requestFocus(inputPhoneFocusNode);
+              }
+            } else {
+              // Foca no input "Name"
+              FocusScope.of(context).requestFocus(inputNameFocusNode);
+            }
+          },
+          backgroundColor: mainColor,
+          child: const Icon(
+            Icons.save,
+            color: darkColor,
+          ),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              GestureDetector(
+                child: Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: _editedContact.imageDirectory != null
+                          ? FileImage(File(_editedContact.imageDirectory!))
+                          : const AssetImage('images/person.png') as ImageProvider,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const Divider(),
-            buildTextField('Name', TextInputType.name, inputNameController, (String inputValue) {
-              setState(() {
-                _editedContact.name = inputValue;
-              });
-            }, inputNameFocusNode),
-            const Divider(),
-            buildTextField('E-Mail', TextInputType.emailAddress, inputEmailController, (String inputValue) {
-              setState(() {
-                _editedContact.email = inputValue;
-              });
-            }, inputEmailFocusNode),
-            const Divider(),
-            buildTextField('Phone number', TextInputType.phone, inputPhoneController, (String inputValue) {
-              setState(() {
-                _editedContact.phone = inputValue;
-              });
-            }, inputPhoneFocusNode),
-          ],
+              const Divider(),
+              buildTextField('Name', TextInputType.name, inputNameController, (String inputValue) {
+                setState(() {
+                  _editedContact.name = inputValue;
+                });
+              }, inputNameFocusNode),
+              const Divider(),
+              buildTextField('E-Mail', TextInputType.emailAddress, inputEmailController, (String inputValue) {
+                setState(() {
+                  _editedContact.email = inputValue;
+                });
+              }, inputEmailFocusNode),
+              const Divider(),
+              buildTextField('Phone number', TextInputType.phone, inputPhoneController, (String inputValue) {
+                setState(() {
+                  _editedContact.phone = inputValue;
+                });
+              }, inputPhoneFocusNode),
+            ],
+          ),
         ),
       ),
     );
   }
-}
 
-Widget buildTextField(
-    String labelText, TextInputType keyboardType, TextEditingController inputController, Function onChange, FocusNode inputFocusNode) {
-  return TextField(
-    controller: inputController,
-    focusNode: inputFocusNode,
-    onChanged: (value) {
-      onChange(value);
-    },
-    keyboardType: keyboardType,
-    decoration: InputDecoration(
-      labelText: labelText,
-      border: const OutlineInputBorder(),
-    ),
-  );
+  // Uma função que intercepta a ação de "page pop"
+  Future<bool> _requestPop() async {
+    if (_isEditedContact) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: darkLightColor,
+            titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
+            title: const Text('Discard changes?'),
+            content: const Text('If you exit the changes will be lost.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Primeiro pop fecha o Alert e o segundo fecha a página de contatos
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                child: const Text('Yes'),
+              ),
+            ],
+          );
+        },
+      );
+      // Impede a ação de pop inicial (para ser feito de forma manual)
+      return Future.value(false);
+    } else {
+      // Mantém a ação de pop inicial
+      return Future.value(true);
+    }
+  }
+
+  // Cria diferentes inputs de textos
+  Widget buildTextField(
+      String labelText, TextInputType keyboardType, TextEditingController inputController, Function onChange, FocusNode inputFocusNode) {
+    return TextField(
+      controller: inputController,
+      focusNode: inputFocusNode,
+      onChanged: (value) {
+        onChange(value);
+      },
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
 }
